a2a8ee9298ee23f92aaa27b9d6169d4c80fcf271
hieugc <89437907+hieugc@users.noreply.github.com>
2021-10-04 10:31:09 +0700

    contact us
    contact us page

---
+++ myScript.js
@@ -0,0 +1,16 @@
+function click_Checkbox() {
+    if(window.innerWidth <= 880 ||  screen.width <= 880)
+        if(document.getElementById('check').checked ==  true) {
+            document.getElementById('main').style.left = '0%';
+        }
+        else{
+            document.getElementById('main').style.left = '-100%';
+        }
+    else{
+        if(document.getElementById('check').checked ==  true) 
+            document.getElementById('mid_nav').style.left = '0%';
+        else
+            document.getElementById('mid_nav').style.left = '-100%';
+    }
+
+}
