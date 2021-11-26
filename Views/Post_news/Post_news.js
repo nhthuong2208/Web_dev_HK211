0507576a1341503b7c9da9007ff3ab5a57a40f1e
dannhh <hongdancuibap@gmail.com>
2021-11-26 14:40:31 +0700

    khong co gi

--- Views/Post_news/Post_news.js
+++ Views/Post_news/Post_news.js
@@ -1,17 +1,10 @@
 document.getElementsByTagName("form")[1].getElementsByTagName("button")[0].onclick = function(){
     var input = document.getElementsByTagName("form")[1].getElementsByTagName("input");
     var text_area = document.getElementsByTagName("form")[1].getElementsByTagName("textarea");
-    // if(input[0].value == "" || input[1].value == "" || input[2].value == "" || text_area[0].value == "" || text_area[1].value == "") return;
-    console.log("1");
-    console.log(input[0].value);
-    console.log(text_area[1].value);
-    console.log( text_area[0].value);
-    console.log(input[1].value)
-    console.log(input[2].value)
-    var day = new Date();
+    if(input[0].value == "" || input[1].value == "" || input[2].value == "" || text_area[0].value == "" || text_area[1].value == "") return;
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-        console.log(this.responseText);
+      console.log(this.responseText);
       document.getElementsByTagName("form")[1].reset();
     };
 
